9b807f77d2f504384a745a56ec0a9fb73ff8aa08
martinconnarty <martinconnarty@hotmail.com>
2023-03-05 07:39:23 +0000

    Add files via upload

--- SubnetCalculator/getIPsFromCIDR.js
+++ SubnetCalculator/getIPsFromCIDR.js
@@ -1,6 +1,24 @@
+
+//Optional - if you are using in a form
+function getIPs(){
+    
+    let subnet = document.forms["myForm"]["subnet"].value; 
+    
+       let ips=[];
+       let ip="";
+       ips=getIPsFromCIDR(subnet);              
+       ips.forEach(resultsprint)
+       function resultsprint(ip) {
+                    document.getElementById('IPs').innerHTML += "<br/>"+ip;
+        }
+ 
+}
+
+
 //getIPsFromCIDR will generate an array of IP addresss from a provided CIDR Range
-getIPsFromCIDR(net) {
-                ipAddresses=[];
+function getIPsFromCIDR(net) {
+      
+                let ipAddresses=[];
                 const regexp= /^(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\/(\d{1,2})$/;
                 const match=net.match(regexp);
                 let a=`${match[1]}`;
@@ -12,7 +30,7 @@ getIPsFromCIDR(net) {
                 let octetmod=mask%8;
                 let octetnumber=Math.floor(mask/8);
                 let ipaddress="";
-                let octect="";
+                let octet="";
                 
                 if (mask<=8)
                 {
@@ -20,33 +38,37 @@ getIPsFromCIDR(net) {
                 }
                 
                 if (octetnumber==3){
-                  octet=a;
+                  octet=d;
                 }
                 else if (octetnumber==2){
-                  octet=b;
-                }
-                else if (octetnumber==1){
                   octet=c;
                 }
+                else if (octetnumber==1){
+                  octet=b;
+                }
                 else if (octetnumber==0){
-                  octet=d;
-                }           
+                  octet=a;
+                }
+                         
             
                 block=(2**(8-octetmod));
                 numberofblocks=Math.floor(octet/block);
+                console.log(`numberofblocks=${numberofblocks} octectmod=${octetmod} mask=${mask} octect=${octet} block=${block}`)
                 network=block*numberofblocks;
                 firsthost=network+1;
                 broadcast=network+(block-1);
                 //Fourth octet
-                if (octetnumber==3){                    
+                if (octetnumber==3){         
+                    console.log(`octectnumber=${octetnumber} firsthost=${firsthost} network=${network} broadcast=${broadcast}`)           
                     for (i=firsthost;i<broadcast;i++){
                             ipaddress=`${a}.${b}.${c}.${i}`; 
-                            ipAddresses.push(ipaddress);
-                    }                    
+                            ipAddresses.push(ipaddress);   
+                        }                    
                 }
                 //Third octet
                 else if (octetnumber==2){
-                    for (i=firsthost;i<=broadcast;i++){
+                    console.log(`octectnumber=${octetnumber} firsthost=${firsthost} network=${network} broadcast=${broadcast}`)
+                    for (i=network;i<=broadcast;i++){
                         for (j=0;j<255;j++){
                             ipaddress=`${a}.${b}.${i}.${j}`;                      
                             ipAddresses.push(ipaddress);                         
@@ -55,8 +77,9 @@ getIPsFromCIDR(net) {
                     
                 }
                 //Second octet
-                else if (octetnumber==3){
-                    for (i=firsthost;i<broadcast;i++){
+                else if (octetnumber==1){
+                    console.log(`octectnumber=${octetnumber} firsthost=${firsthost} network=${network} broadcast=${broadcast}`)
+                    for (i=network;i<broadcast;i++){
                         for (j=0;j<255;j++){
                             for (k=0;k<255;k++){
                             ipaddress=`${a}.${i}.${j}.${k}`;
@@ -66,17 +89,18 @@ getIPsFromCIDR(net) {
                     }      
                 }
                 //First octet
-                else if (octetnumber==0){                    
-                    for (i=firsthost;i<broadcast;i++){
+                else if (octetnumber==0){  
+                    console.log(`octectnumber=${octetnumber} firsthost=${firsthost} network=${network} broadcast=${broadcast}`)                  
+                    for (i=network;i<broadcast;i++){
                         for (j=0;j<255;j++){
                             for (k=0;k<255;k++){
                                 for (l=0;l<255;l++){                                
                                 ipaddress=`${i}.${j}.${k}.${l}`;
                                 ipAddresses.push(ipaddress);
+                                }   
                             }
-                        }
                         }
                     }
                 }
         return ipAddresses
-            }
+}
